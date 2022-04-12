fbd7ffe8edea7ca2e1d55c0e0a465767d5814f1c
millimetro <millimetro.art@gmail.com>
2022-04-12 19:11:23 +0200

    feat: added Manifesto page

---
+++ pages/manifesto.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-content text">
+    <p>
+      MULTIVERSE vuole esseer un HUB musicale con l'obbiettivo di supportare la
+      scena elettronica italiana. Una scena ormai frammentata che, figlia di un
+      movimento 'culturale underground', è diventata di matrice 'mainstream'
+      poiché soffocata dall'esigenza economica.
+    </p>
+
+    <p>
+      Il nostro obbiettivo principale è quello di fornire un luogo etereo dove
+      gli amanti della musica elettronica possano avere la possibilità di uno
+      scambio culturale e di esperienze, dove possano trovare contenti utili
+      alla crescita artistica e un ausilio allo sviluppo dei propri progetti.
+    </p>
+
+    <p>
+      Un movimento vive di creazione di connessioni sane attraverso la
+      condivisione di progetti, di idee e -soprattutto- di un'apertura mentale.
+    </p>
+
+    <p>MULTIVERSE nasce e rimarrà indipendente.</p>
+
+    <p>
+      Rimarrà un patrimonio comune di tutti coloro che parteciperanno
+      attivamente e che ne vorranno sposare i principi.
+    </p>
+
+    <p>Save the Italian Electronic Scene</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-content {
+  height: 100vh;
+}
+
+.text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 50ch;
+  margin: auto;
+  text-align: center;
+}
+</style>
