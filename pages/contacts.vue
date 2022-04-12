2bcc33c9aa073f50d3a9c237409fa838e79686f7
Michele <sarubbi.michele@gmail.com>
2022-04-12 18:38:54 +0200

    feat: added Contacts page

---
+++ pages/contacts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main-content">
+    <div class="contacts">
+      <a href="mailto:">multiverse.controlroom@gmail.com</a>
+      <a href="https://www.instagram.com/multiverseclubit/">instagram</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-content {
+  height: 100vh;
+}
+.contacts {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 50ch;
+  margin: auto;
+  text-align: center;
+}
+</style>
